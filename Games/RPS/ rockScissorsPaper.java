376c4039f38dc90840112a0475dfc3d9ddae8040
Bilal Arslan <bilal.arslan1993@hotmail.com>
2019-05-12 15:25:33 +0300

    format code and files

--- Games/RPS/ rockScissorsPaper.java
+++ Games/RPS/ rockScissorsPaper.java
@@ -12,79 +12,68 @@ public class rpsGameTR {
 	public static int draw;
 	public static int newgame;
 
-	static void checkIt()  {
+	static void checkIt() {
 		if (rps == 1 && robot == 1)	{
 			System.out.println("Rakibiniz de TAŞ gösterdi. Malesef Berabere Kaldınız.");
 			draw++;
-		}
-		else if (rps == 1 && robot == 2)	{
+		} else if (rps == 1 && robot == 2) {
 			System.out.println("Rakibiniz KAĞIT gösterdi. YENİLDİNİZ !");
 			defeat++;
-		}
-		else if (rps == 1 && robot == 3)	{
+		} else if (rps == 1 && robot == 3) {
 			System.out.println("Rakibiniz MAKAS gösterdi. TEBRİKLER KAZANDINIZ !");
 			win++;
-		}
-		else if (rps == 2 && robot == 1)	{
+		} else if (rps == 2 && robot == 1) {
 			System.out.println("Rakibiniz TAŞ gösterdi. TEBRİKLER KAZANDINIZ !");
 			win++;
-		}
-		else if (rps == 2 && robot == 2)	{
+		} else if (rps == 2 && robot == 2) {
 			System.out.println("Rakibiniz de KAĞIT gösterdi. Malesef Berabere Kaldınız.");
 			draw++;
-		}
-		else if (rps == 2 && robot == 3)	{
+		} else if (rps == 2 && robot == 3) {
 			System.out.println("Rakibiniz MAKAS gösterdi. YENİLDİNİZ !");
 			defeat++;
-		}
-		else if (rps == 3 && robot == 1)	{
+		} else if (rps == 3 && robot == 1) {
 			System.out.println("Rakibiniz TAŞ gösterdi. YENİLDİNİZ !");
 			defeat++;
-		}
-		else if (rps == 3 && robot == 2)	{
+		} else if (rps == 3 && robot == 2) {
 			System.out.println("Rakibiniz KAĞIT gösterdi. TEBRİKLER KAZANDINIZ !");
 			win++;
-		}
-		else	{
+		} else {
 			System.out.println("Rakibiniz de MAKAS gösterdi. Malesef Berabere Kaldınız.");
 			draw++;
 		}
 		
 	}
 
-	public static void main(String[] args)	{
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		Random random = new Random();
 		int round;
 		int choise;
-		
+
 		System.out.println("Lutfen Isminizi Giriniz = ");
 		String name = input.nextLine();
-		
+		System.out.println("Taş - Kağıt - Makas Oyununa Hoşgeldin " + name + " :)");
+
 		while(true)	{
-			System.out.println("Taş - Kağıt - Makas Oyununa Hoşgeldin " + name + " :)");
 			System.out.println("Oyunu Kaç El Oynamak İstersin ?");
 			round = input.nextInt();
 		
 			win = 0;
 			draw = 0;
 			defeat = 0;
-		
-		
-			for (int x = 0; x<round; x++)	{
-				robot = random.nextInt(3) + 1; ////taş(1), kağıt(2), makas(3)
+
+			for (int x = 0; x < round; x++)	{
+				robot = random.nextInt(3) + 1; // taş(1), kağıt(2), makas(3)
 				System.out.println("Seçimini Yap, TAŞ[1] KAĞIT[2] MAKAS[3]");
 				rps = input.nextInt();
-				if (rps == 1)	{
+
+				if (rps == 1) {
 					System.out.println("Seçimin = TAŞ");
-				}
-				else if (rps == 2)	{
+				} else if (rps == 2) {
 					System.out.println("Seçimin = KAĞIT");
-				}
-				else if	(rps == 3)	{
+				} else if	(rps == 3) {
 					System.out.println("Seçimin = MAKAS");
-				}
-				else	{
+				} else {
 					System.out.println("Hatalı Seçim Yaptınız !");
 					round++;
 				}
@@ -93,17 +82,15 @@ public class rpsGameTR {
 			}
 			System.out.println("GALİBİYET = " + win + " " + "BERABERLİK = " + draw + " " + "YENİLGİ = " + defeat);
 			System.out.println();
-			System.out.println("Tekrar oynamak istermisiniz ? EVET[1] HAYIR[2]");
+			System.out.println("Tekrar oynamak istermisiniz? EVET[1] HAYIR[2]");
 			newgame = input.nextInt();
-			
-			if (newgame == 1)	{
+
+			if (newgame == 1) {
 				
-			}
-			else if (newgame == 2)	{
+			} else if (newgame == 2) {
 				System.out.println("İYİ GÜNLER " + name + " :)");
 				break;
-			}
-			else	{
+			} else {
 				System.out.println("Hatalı seçimde bulundunuz. İyi günler.");
 				break;
 			}
--- Games/diceGame/diceGame.java
+++ Games/diceGame/diceGame.java
@@ -5,38 +5,28 @@ import java.util.Random;
 
 public class diceGame {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		Random random = new Random();
-		
+
 		int dice;
 		int control;
 		
-		while(true)
-		{
-			dice = random.nextInt(6) + 1;
-			
-			System.out.println("Do you want to throw a dice ? YES[1] NO[2]");
+		while (true) {
+			System.out.println("Do you want to throw a dice? YES[1] NO[2]");
 			control = input.nextInt();
 			
-			if(control == 1)
-			{
+			if (control == 1) {
+				dice = random.nextInt(6) + 1;
 				System.out.println("The value of your dice = " + dice);
 				System.out.println();
-			}
-			else if(control == 2)
-			{
+			} else if (control == 2) {
 				System.out.println("OK. Have a nice day :)");
 				break;
-			}
-			else
-			{
+			} else {
 				System.out.println("Wrong Choice :(");
 				System.out.println();
 			}
 		}
-
 	}
-
 }
